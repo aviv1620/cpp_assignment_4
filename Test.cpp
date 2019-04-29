a8954c7422584bdf54b99d797d5c1cb617fe7c7d
aviv <aviv1620@gmail.com>
2019-04-29 17:16:41 +0300

    test

--- Test.cpp
+++ Test.cpp
@@ -49,6 +49,58 @@ int main() {
 		}
 		
 		//my test:
+		//empty value
+		testcase.setname("test empty value");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("",""), "0,0");
+		ConstantChooser cempty{""};
+		ConstantGuesser gempty{""};
+		testcase.CHECK_EQUAL(play(cempty, gempty, 0, 1), 1); // guesser wins	
+		
+		//single value
+		testcase.setname("test single value");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("1","0"), "0,0");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("1","1"), "1,0");
+		ConstantChooser c1{"1"};
+		ConstantGuesser g0{"0"};
+		ConstantGuesser g1{"1"};
+		testcase.CHECK_EQUAL(play(c1, g1, 1, 1), 1); // guesser wins	
+		testcase.CHECK_EQUAL(play(c1, g0, 1, 5), 6); // guesser loses 
+		
+		//bull
+		testcase.setname("test bull");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("00","11"), "0,0");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("10","11"), "1,0");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("01","11"), "1,0");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("11","11"), "2,0");
+		ConstantChooser c11{"11"};
+		ConstantGuesser g11{"11"};
+		testcase.CHECK_EQUAL(play(c11, g11, 2, 1), 1);	// guesser wins
+		
+		//Pgia
+		testcase.setname("test Pgia");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("11","12"), "1,1");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("12","12"), "2,0");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("21","12"), "0,2");
+		testcase.CHECK_OUTPUT(calculateBullAndPgia("22","12"), "1,0");
+		ConstantChooser c12{"12"};
+		ConstantGuesser g12{"12"};
+		testcase.CHECK_EQUAL(play(c12, g12, 2, 1), 1);	// guesser wins
+		
+		//SmartGuesser two digit
+		testcase.setname("test smart SmartGuesser - two digit");
+		ConstantChooser c99{"99"};
+		ConstantChooser c00{"00"};
+		testcase.CHECK_EQUAL(play(c99, smarty, 2, 100)<=10, true);
+		
+		//SmartGuesser one digit
+		testcase.setname("test smart SmartGuesser - one digit");
+		ConstantChooser c9{"9"};
+		ConstantChooser c0{"0"};
+		testcase.CHECK_EQUAL(play(c9, smarty, 1, 100)<=10, true);
+		
+		//SmartGuesser empty
+		testcase.setname("test smart SmartGuesser - Gues empty");
+		testcase.CHECK_EQUAL(play(cempty, smarty, 0, 100)<=10, true);
 
     grade = testcase.grade();
 	} else {
--- calculate.cpp
+++ calculate.cpp
@@ -12,15 +12,16 @@ namespace bullpgia {
 		uint GL = guess.length(); // GL = guess length.
 		
 		if( CL != GL) //Illegal guess and choice.
-			throw string( "choice length is " + to_string(CL) + "but guess length id" + to_string(GL) );
+			throw string( "choice length is " + to_string(CL) + " but guess length is " + to_string(GL) );
 		
 		//calculate.
 		Replay replay;
 		
 		for(uint i=0; i < CL ;i++){
+			//cout << choice[i] << " result " << choice.find(guess[i]) << endl;
 			if(guess[i] == choice[i])//have Bull.
 				replay.bull++;
-			else if( choice.find(guess[i]) != string::npos)//have Pgia.
+			else if( choice.find(guess[i]) != string::npos)//have Pgia. //have problem with digit 0;
 				replay.pgia++;
 		}
 		
